e59dad7fb7df823f8843d66bfcb99db235a2f91e
just <injune7777@gmail.com>
2015-09-01 23:16:52 +0800

    update

---
+++ TrickCameraSocialPlatform/PanoViewerControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PanoViewerControllerViewController.h
+//  TrickCameraSocialPlatform
+//
+//  Created by Just on 2015/9/1.
+//  Copyright (c) 2015年 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PanoViewerControllerViewController : UIViewController
+
+@end
