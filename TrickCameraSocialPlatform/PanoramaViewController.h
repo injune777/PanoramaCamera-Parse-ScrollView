b24b2a7c594c70c25b48465276382dfa19497954
just <injune7777@gmail.com>
2015-09-02 14:06:12 +0800

    upgrade

---
+++ TrickCameraSocialPlatform/PanoramaViewController.h
@@ -0,0 +1,13 @@
+//
+//  PanoramaViewController.h
+//  TrickCameraSocialPlatform
+//
+//  Created by Just on 2015/9/2.
+//  Copyright (c) 2015年 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PanoramaViewController : UIViewController
+
+@end
---
+++ TrickCameraSocialPlatform/PanoramaViewController.m
@@ -0,0 +1,37 @@
+//
+//  PanoramaViewController.m
+//  TrickCameraSocialPlatform
+//
+//  Created by Just on 2015/9/2.
+//  Copyright (c) 2015年 OSX. All rights reserved.
+//
+
+#import "PanoramaViewController.h"
+
+@interface PanoramaViewController ()
+
+@end
+
+@implementation PanoramaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
